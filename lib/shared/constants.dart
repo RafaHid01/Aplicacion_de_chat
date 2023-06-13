e81b2bf69558749dac3797fa4fb1eb7774bbe5c2
RafaHid01 <135166362+RafaHid01@users.noreply.github.com>
2023-06-13 20:17:15 -0300

    subida de aplicacion

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  static String appId= "1:563678834732:web:68267c9a59f7b7b6cbe189";
+  static String apiKey= "AIzaSyA5r7tIZtbUkdv4GtTjVNbB84x9a4fp_QQ";
+  static String messagingSenderId= "563678834732";
+  static String projectId= "appchat-37520";
+  final primaryColor = Color.fromARGB(255, 43, 214, 57);
+}
